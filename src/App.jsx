fd9ff06367c3cc346297f92be018703cb5247a15
EzozaMukhamedova <ezoza.muxamedova@gmail.com>
2025-02-04 05:54:32 +0900

    input p

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+// import { useState, useEffect } from "react";
+// import "./App.css";
+
+// function App() {
+//   const [text, setText] = useState("Hello, World!");
+
+//   return (
+//     <div>
+//       <p>{text ? text : "p"}</p>
+//       <input
+//         type="text"
+//         placeholder="nimadur yozing..."
+//         value={text}
+//         onChange={(e) => {
+//           setText(e.target.value);
+//         }}
+//       />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [text, setText] = useState("");
+  const [displayedText, setDisplayedText] = useState("");
+
+  const handleInputChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const handleButtonClick = () => {
+    setDisplayedText(text);
+  };
+
+  return (
+    <div>
+      <p>{displayedText ? displayedText : "Yozganingiz chiqadi!"}</p>
+      <input
+        type="text"
+        placeholder="nimadur yozing..."
+        value={text}
+        onChange={handleInputChange}
+      />
+      <button onClick={handleButtonClick}>Bosing</button>
+    </div>
+  );
+}
+
+export default App;
